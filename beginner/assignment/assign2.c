e7f6e158a65fdba8622cf91d40e15dea3b9d57cf
Priyanka096 <34641098+Priyanka096@users.noreply.github.com>
2017-12-18 14:46:25 +0530

    Create assign2.c

---
+++ beginner/assignment/assign2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	char ch[25]="Sachin Ramesh Tendulkar";
+	char ch1[15]="Little master";
+	long int a=18426;
+	float f=44.83;
+	int b=463;
+	printf("NAME:%s\n",ch);
+	printf("NICK-NAME:%s\n",ch1);
+	printf("RUNS SCORED:%d\n",a);
+	printf("BATTING AVERAGE:%f\n",f);
+	printf("ODI-MATCHES:%d\n",b);
+	
+	return 0;
+}
