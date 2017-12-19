5d57f20740f989c22303ddb6e6aa1020be981969
Priyanka096 <34641098+Priyanka096@users.noreply.github.com>
2017-12-19 21:18:52 +0530

    Create palindrome.c

---
+++ codekata/beginner/set2/palindrome.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void) {
+int n,temp,reverse=0;
+scanf("%d",&n);
+temp=n;
+while(n!=0)
+{
+	reverse=reverse*10;
+            reverse+=n%10;
+            n=n/10;
+	
+}
+if(temp==reverse)
+{
+	printf("The number is a palindrome.\n");
+}
+else
+{
+	printf("\n The number is not a palindrome.");
+}
+
+   
+	return 0;
+}
+
+
