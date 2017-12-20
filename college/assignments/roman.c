0060e2a17e7cea73082d2947c63bfcea1eacb657
Priyanka096 <34641098+Priyanka096@users.noreply.github.com>
2017-12-20 10:39:05 +0000

    Create roman.c

---
+++ college/assignments/roman.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+ int a[20],s,i=0,j,k;
+ char roman[5];
+ printf("Enter Only alphabets I,V,X");
+ scanf("%s",roman);
+ s=strlen(roman);
+ for(i=0;i<s;i++)
+ {
+  if(roman[i]=='I')
+  a[i]=1;
+  else if(roman[i]=='V')
+  a[i]=5;
+  else if(roman[i]=='X')
+  a[i]=10;
+  else
+  {
+printf("\n Please enter correct roman");
+  	
+  }
+}
+k=a[s-1];
+for(i=s-1;i>0;i--)
+{
+  if(a[i]>a[i-1])
+  k=k-a[i-1];
+  else if(a[i]==a[i-1] || a[i]<a[i-1])
+  k=k+a[i-1];
+}
+printf("\nEquivalent Number is %d ",k);
+return 0;
+
+}
