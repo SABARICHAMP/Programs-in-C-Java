1c946c51318b60d4162ef49420a93fd854e8d8cf
Priyanka096 <34641098+Priyanka096@users.noreply.github.com>
2018-01-12 11:26:21 +0530

    Update max_element_array.c

--- codekata/beginner/set3/max_element_array.c
+++ codekata/beginner/set3/max_element_array.c
@@ -1,32 +1,23 @@
 #include <stdio.h>
-int max(int arr[],int n)
-{
-int maximum,temp=0;
-for(int i=0;i<n;i++)
-{
-scanf("%d",&arr[i]);
-}
-for(int i=0;i<n;i++)
-{
-for(int j=i+1;j<n;j++)
-{
-if(arr[i]<arr[j])			
-{
-temp=arr[i];
-arr[i]=arr[j];
-arr[j]=temp;
-}
-}
-}
-maximum=arr[0];
-return maximum;
-}
-int main(void) 
-{
-int n;
-int maximum,arr[15];
+int main(void) {
+int arr[20],n,temp=0;
 scanf("%d",&n);
-maximum=max(arr,n);
-printf("%d\n",maximum);
+for(int i=0;i<n;i++)
+{
+	scanf("%d\n",&arr[i]);
+}
+for(int i=0;i<n;i++)
+{
+	for(int j=i+1;j<n;j++)
+	{
+		if(arr[i]<arr[j])
+		{
+			temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+		}
+	}
+}
+printf("%d\n",arr[0]);
 return 0;
 }
